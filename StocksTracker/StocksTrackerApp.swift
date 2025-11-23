99d0555a454d458ed368ecc701190020131f731b
Ammar Rajab <ammar.earth@gmail.com>
2025-11-23 22:50:43 +0400

    Initial Commit

---
+++ StocksTracker/StocksTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTrackerApp.swift
+//  StocksTracker
+//
+//  Created by Ammar on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StocksTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
